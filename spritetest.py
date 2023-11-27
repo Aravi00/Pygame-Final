c24e4dba16839b2552ae744ef174761b7b3cad1d
Aravi00 <147725768+Aravi00@users.noreply.github.com>
2023-11-27 11:13:58 -0500

    more spritesheet

--- spritetest.py
+++ spritetest.py
@@ -8,10 +8,10 @@ window = py.display.set_mode((windowwidth,windowheight))
 py.display.set_caption("Arav's Spiderman Game")
 clock = py.time.Clock()
 
-miles = py.image.load("miles.png")
-scale = 1.2
+miles = py.image.load("milesjump.png")
+scale = 1
 milesm = py.transform.scale_by(miles,scale)
-milesloc = py.Rect(0*scale,0*scale,50*scale,40*scale)
+milesloc = py.Rect(0*scale,0*scale,40*scale,60*scale)
 milesnum = 0
 milesframerate = 30
 framecount = 0
@@ -22,16 +22,16 @@ while True:
         break
     key = py.key.get_pressed()
     if framecount % milesframerate ==0:
-        if(milesnum<6):
+        if(milesnum<5):
             milesnum+=1
-            milesloc.x += milesloc.width
+            milesloc.x += 50
         else:
             milesnum=0
             milesloc.x =0
     framecount+=1
     window.fill("white")
     #draw here 
-    window.blit(milesm,(0,0),milesloc)
+    window.blit(milesm,(10,0),milesloc)
     py.display.flip()
     clock.tick(60)
 py.quit()
--- v2.py
+++ v2.py
@@ -12,7 +12,7 @@ nightbg = py.image.load("nightbg.jpg")
 nightbg2 = py.image.load("nightbg 3.jpg")
 daybg = py.image.load("day bg.png")
 foreground = py.image.load("foreground.png")
-miles = py.image.load("miles.png")
+miles = py.image.load("milesjump.png")
 
 nightsurface = py.transform.scale_by(nightbg2,1)
 fsurface = py.transform.scale_by(foreground,2)
@@ -22,12 +22,32 @@ nightx2 = size
 #loop
 
 yspeed = 2
-rect = py.Rect(100,150,30,40)
+rect = py.Rect(100,150,30,50)
 blockpos = py.Rect(0,random.randint(300,400),random.randint(250,400),200)#left,top,width,height
 blockpos.height = windowheight-blockpos.top
 blockpos2 = py.Rect(blockpos.right+random.randint(100,300),random.randint(250,400),random.randint(250,500),200)#x distance has to be >=300
 blockpos2.height = windowheight-blockpos2.top
 blocks = [blockpos,blockpos2]
+
+scale = 1
+milesm = py.transform.scale_by(miles,scale)
+milesloc = py.Rect(0*scale,70*scale,50*scale,50*scale)
+milesnum = 0
+milesframerate = 10
+framecount = 0
+
+def animate(ypos,framewidth,numframes):
+    global framecount,milesframerate,milesnum
+    milesloc.y = ypos
+    if framecount % milesframerate ==0:
+        if(milesnum<numframes-1):
+            milesnum+=1
+            milesloc.x += framewidth
+        else:
+            milesnum=0
+            milesloc.x =0
+    framecount+=1
+        
 while True:
     ev = py.event.poll()
     if ev.type == py.QUIT:
@@ -41,13 +61,15 @@ while True:
         nightx2 -=2
         for i in range(len(blocks)):
             blocks[i].left -=15
+        animate(70,milesloc.width,6)
+
     print(" ")
     yspeed+=0.25
     rect.top+=yspeed
     for i in range(len(blocks)):
         if rect.bottom+1 > blocks[i].top and rect.left > blocks[i].left and rect.left < blocks[i].right:#if py.Rect.colliderect(rect,blocks[i]):
             yspeed = 0
-            rect.top = blocks[i].top-40           
+            rect.top = blocks[i].top-50           
             if (key[py.K_SPACE]):
                 yspeed = -7.5
             break
@@ -55,6 +77,11 @@ while True:
             print("edge")
             if key[py.K_w]:
                 rect.top+=10
+    if yspeed >0:
+        animate(0,40,7)
+    else:
+        milesloc.x = 0
+        milesloc.y = 70
         
         
     if blocks[0].right < 0:
@@ -70,12 +97,13 @@ while True:
      #x,y,w,h
     #draw here 
     window.blit(nightsurface,(nightx,0))
-    window.blit(miles,(0,0))
     window.blit(nightsurface,(nightx2,0))
     #window.blit(fsurface,(nightx*2-40,175))
     for i in range(len(blocks)):
         py.draw.rect(window,(255,0,0),blocks[i])
-    py.draw.rect(window,(0,0,0),rect)
+    #py.draw.rect(window,(0,0,0),rect)
+    window.blit(milesm,(rect.x,rect.y),milesloc)
     py.display.flip()
     clock.tick(60)
 py.quit()
+
